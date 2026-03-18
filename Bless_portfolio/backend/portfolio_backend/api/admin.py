e13b204567089bf4b14212f15a3a4da946f11ebf
njogubless <pnjogubless@gmail.com>
2026-03-18 15:37:31 +0300

    defining the messages module registered on admin

--- Bless_portfolio/backend/portfolio_backend/api/admin.py
+++ Bless_portfolio/backend/portfolio_backend/api/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
+from .models import Project, Message
 
-from .models import Project
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display  = ['name', 'email', 'subject', 'sent_at', 'is_read']
+    list_filter   = ['is_read']
+    readonly_fields = ['name', 'email', 'subject', 'body', 'sent_at']
+    # readonly so you can't accidentally edit what someone sent you
 
 admin.site.register(Project)
