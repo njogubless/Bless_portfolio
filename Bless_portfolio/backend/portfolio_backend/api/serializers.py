4b3e0429fa65806aeeaf9a605aa26b740ba17cbd
njogubless <pnjogubless@gmail.com>
2026-03-18 15:38:11 +0300

    defining the serializers for read/write protection

--- Bless_portfolio/backend/portfolio_backend/api/serializers.py
+++ Bless_portfolio/backend/portfolio_backend/api/serializers.py
@@ -1,5 +1,7 @@
+#from email.message import Message
+
 from rest_framework import serializers
-from .models import Project
+from .models import Project, Message
 
 # A serializer converts a Python object (your Project model)
 # into JSON that React can read — and vice versa
@@ -7,3 +9,10 @@ class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model  = Project
         fields = ['id', 'name', 'tech', 'description', 'github_url', 'icon', 'color', 'order', 'created_at']
+
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model  = Message
+        fields = ['id', 'name', 'email', 'subject', 'body', 'sent_at']
