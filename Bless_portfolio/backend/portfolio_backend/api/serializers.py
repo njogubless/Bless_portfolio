11c92849cd8da52c2eaf853b133f7119406d4088
njogubless <pnjogubless@gmail.com>
2026-03-23 14:18:25 +0300

    feat(serializers): add BlogPostListSerializer and BlogPostDetailSeriliazer

--- Bless_portfolio/backend/portfolio_backend/api/serializers.py
+++ Bless_portfolio/backend/portfolio_backend/api/serializers.py
@@ -1,7 +1,7 @@
 
 
 from rest_framework import serializers
-from .models import Project, Message
+from .models import BlogPost, Project, Message
 
 
 class ProjectSerializer(serializers.ModelSerializer):
@@ -15,3 +15,28 @@ class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model  = Message
         fields = ['id', 'name', 'email', 'subject', 'body', 'sent_at']
+
+
+class BlogPostListSerializer(serializers.ModelSerializer):
+    reading_time = serializers.ReadOnlyField()
+    tags_list = serializers.ReadOnlyField()
+
+    class Meta:
+        model = BlogPost
+        fields =[
+            'id', 'title', 'slug', 'excerpt',
+            'catergory', 'tags_list', 'cover',
+            'reading_time','created_at',
+        ]
+ 
+class BlogPostDetailSerializer(serializers.ModelSerializer):
+    reading_time = serializers.ReadOnlyField()
+    tags_list = serializers.ReadOnlyField()
+
+    class Meta:
+        model = BlogPost
+        fields =[
+            'id', 'title', 'slug', 'excerpt',
+            'content', 'catergory', 'tags_list',
+            'cover', 'reading_time','created_at',
+        ]
