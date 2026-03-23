e78442cd89ecc2b9de600debc963f14b383ae1d0
njogubless <pnjogubless@gmail.com>
2026-03-23 14:19:39 +0300

    feat(views): add BlogPostListview with filter and BlogPostDetailsView

--- Bless_portfolio/backend/portfolio_backend/api/views.py
+++ Bless_portfolio/backend/portfolio_backend/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics, permissions
-from .models import Project, Message
-from .serializers import ProjectSerializer, MessageSerializer
+from .models import Project, Message , BlogPost
+from .serializers import ProjectSerializer, MessageSerializer, BlogPostDetailSerializer, BlogPostListSerializer
 
 class ProjectListView(generics.ListCreateAPIView):
     queryset         = Project.objects.all()
@@ -24,3 +24,23 @@ class MessageListView(generics.ListAPIView):
     serializer_class = MessageSerializer
 
     permission_classes = [permissions.IsAuthenticated]
+
+
+class BlogPostListView(generics.ListAPIView):
+    serializer_class = BlogPostListSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+    def get_queryset(self):
+        queryset = BlogPost.objects.filter(published = True)
+        category = self.request.query_params.get('category')
+        if category:
+            queryset = queryset.filter(category=category)
+        return queryset
+
+
+class BlogPostDetailView(generics.RetrieveAPIView):
+    serialzer_class = BlogPostDetailSerializer
+    permission_classes = [permissions.AllowAny]
+    queryset = BlogPost.objects.filter(published=True)
+    lookup_field = 'slug'
