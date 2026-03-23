8a2eaca731278f2e3e7544e0108a915afead975e
njogubless <pnjogubless@gmail.com>
2026-03-23 12:09:59 +0300

    feat: adding the footer on the app.jsx

--- Bless_portfolio/frontend/src/App.jsx
+++ Bless_portfolio/frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Background from './components/Background'
 import Navbar     from './components/Navbar'
+import Footer     from './components/Footer'
 import Home       from './pages/Home'
 import About      from './pages/About'
 import Work       from './pages/Work'
@@ -8,15 +9,18 @@ import Contact    from './pages/Contact'
 
 function App() {
   return (
-    <div style={{ position: 'relative', minHeight: '100vh' }}>
+    <div style={{ position: 'relative', minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
       <Background />
       <Navbar />
-      <Routes>
-        <Route path="/"        element={<Home />}    />
-        <Route path="/about"   element={<About />}   />
-        <Route path="/work"    element={<Work />}    />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <div style={{ flex: 1 }}>
+        <Routes>
+          <Route path="/"        element={<Home />}    />
+          <Route path="/about"   element={<About />}   />
+          <Route path="/work"    element={<Work />}    />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+      <Footer />
     </div>
   )
 }
