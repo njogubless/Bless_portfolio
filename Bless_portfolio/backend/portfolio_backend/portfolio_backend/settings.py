dc3424cbd6cd2e2dad4ecbaae022a19435532310
njogubless <pnjogubless@gmail.com>
2026-03-27 22:29:06 +0300

    fix: email messaging configuration on the setting.py file

--- Bless_portfolio/backend/portfolio_backend/portfolio_backend/settings.py
+++ Bless_portfolio/backend/portfolio_backend/portfolio_backend/settings.py
@@ -148,3 +148,11 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'njogubless2@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = 'njogubless2@gmail.com'
